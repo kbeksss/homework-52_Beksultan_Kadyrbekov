1f55316989de4e50fafde60d1b679f73f91ddf7c
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-11-11 10:48:37 +0600

    Made a method that gets random numbers and pushes to an array

--- src/Components/RandomNumbers/RandomNumbers.js
+++ src/Components/RandomNumbers/RandomNumbers.js
@@ -3,16 +3,52 @@ import Numbers from "../Numbers/Numbers";
 import './RandomNumbers.css'
 
 class RandomNumbers extends React.Component{
+    state = {
+        randomNs: []
+    };
+    randomNumber = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+    newNs = () => {
+        const randomNs = [];
+        for (let i = 0; i <= 4;){
+            let number = 0;
+            if(i === 0){
+                number = this.randomNumber(5, 32);
+            } else if(i === 1){
+                number = this.randomNumber(5, 33);
+            } else if(i === 2){
+                number = this.randomNumber(5, 34);
+            } else if(i === 3){
+                number = this.randomNumber(5, 35)
+            } else {
+                number = this.randomNumber(5, 36);
+            }
+
+            if(randomNs.includes(number)){
+                continue;
+            }
+            if(number < randomNs[i-1]){
+                console.log('array' + randomNs[i - 1]);
+                console.log('number' + number);
+                continue;
+            }
+            console.log(i);
+            console.log(number);
+            randomNs.push(number);
+            i++;
+        }
+        console.log(randomNs);
+    };
     render() {
         return(
             <div className="random">
                 <div>
-                    <button className="btn">New numbers</button>
+                    <button className="btn" onClick={this.newNs}>New numbers</button>
                 </div>
-                <Numbers number="5"/>
+                <Numbers number="4"/>
                 <Numbers number="13"/>
                 <Numbers number="23"/>
                 <Numbers number="25"/>
+                <Numbers number="32"/>
             </div>
         )
     }
